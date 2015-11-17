6b552833f73a6f257d6ea06c1775b052b80e215d
jens7841 <jens7841@web.de>
2015-11-17 10:20:52 +0100

    + Parameterloser Konstruktor

--- src/benutzerverwaltung/Benutzerverwaltung.java
+++ src/benutzerverwaltung/Benutzerverwaltung.java
@@ -8,6 +8,12 @@ public class Benutzerverwaltung {
 	private ArrayList<User> benutzer;
 
 	public Benutzerverwaltung(List<User> benutzer) {
+		this.benutzer = new ArrayList<>();
 
+		this.benutzer.addAll(benutzer);
+	}
+
+	public Benutzerverwaltung() {
+		this(new ArrayList<>());
 	}
 }
