48307fa636c18c3853c5774a4813c3583c28a260
jens7841 <jens7841@web.de>
2015-11-17 10:43:31 +0100

    + outstream benutzer daten

--- src/benutzerverwaltung/Benutzerverwaltung.java
+++ src/benutzerverwaltung/Benutzerverwaltung.java
@@ -1,11 +1,14 @@
 package benutzerverwaltung;
 
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Benutzerverwaltung {
 
 	private ArrayList<User> benutzer;
+	private int letzteId;
 
 	public Benutzerverwaltung(List<User> benutzer) {
 		this.benutzer = new ArrayList<>();
@@ -15,6 +18,23 @@ public class Benutzerverwaltung {
 
 	public Benutzerverwaltung() {
 		this(new ArrayList<>());
+	}
+
+	public void schreibeUserDaten(OutputStream out) {
+		PrintWriter writer = new PrintWriter(out);
+		writer.println(letzteId);
+
+		for (User user : benutzer) {
+			StringBuilder builder = new StringBuilder();
+			builder.append(user.getId());
+			builder.append(';');
+			builder.append(user.getName());
+			builder.append(';');
+			builder.append(user.getPasswort());
+			writer.println(builder.toString());
+		}
+
+		writer.flush();
 	}
 
 	public void benutzerRegistrieren(String name, String passwort) {
