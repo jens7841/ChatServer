b1c214a1254adfab0f394ce410d7327863963b45
jens7841 <jens7841@web.de>
2015-11-17 14:01:58 +0100

    test

--- src/benutzerverwaltung/Test.java
+++ src/benutzerverwaltung/Test.java
@@ -1,16 +1,12 @@
 package benutzerverwaltung;
 
-import java.io.FileOutputStream;
-
 public class Test {
 
 	public static void main(String[] args) throws Exception {
-		Benutzerverwaltung ver = new Benutzerverwaltung();
+		Benutzerverwaltung ver = new Benutzerverwaltung("users.csv");
 		ver.benutzerRegistrieren("Test1", "TestPw1");
 		ver.benutzerRegistrieren("Test2", "TestPw2");
 		ver.benutzerRegistrieren("Test3", "TestPw3");
 		ver.benutzerRegistrieren("Test4", "TestPw4");
-
-		ver.schreibeUserDaten(new FileOutputStream("users.csv"));
 	}
 }
