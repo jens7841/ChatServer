756633b57276b16ff6b9eb45fb4a249cc021fb42
clemens961 <clemens961@gmail.com>
2015-11-18 12:49:34 +0100

    +MessageSender()

---
+++ src/server/MessageSender.java
@@ -0,0 +1,15 @@
+package server;
+
+import usermanager.User;
+
+public class MessageSender implements Runnable {
+
+	public MessageSender(User sender, User receiver) {
+		// do magic stuff
+	}
+
+	@Override
+	public void run() {
+
+	}
+}
