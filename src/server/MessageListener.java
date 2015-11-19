3f34ca15963f54000ffea616473e39acafcf7ff9
jens7841 <jens7841@web.de>
2015-11-19 08:47:22 +0100

    einloggen geht

--- src/server/MessageListener.java
+++ src/server/MessageListener.java
@@ -11,10 +11,11 @@ import usermanager.UserException;
 import usermanager.Usermanager;
 
 public class MessageListener extends Thread {
+	public static final int DELIMITER = 0;
 	public static final int END_OF_MESSAGE = 1;
 	public static final int CHAT_MESSAGE = 2;
 	public static final int LOGIN = 3;
-	public static final int ERROR_MESSAGE = 4; // TODO datentyp draus machen?!
+	public static final int ERROR_MESSAGE = 4;
 
 	private Socket socket;
 	private User user;
@@ -22,8 +23,8 @@ public class MessageListener extends Thread {
 
 	public MessageListener(Socket socket, Usermanager usermanager) {
 		this.socket = socket;
-		this.start();
 		this.usermanager = usermanager;
+		this.start();
 	}
 
 	@Override
@@ -46,18 +47,20 @@ public class MessageListener extends Thread {
 
 					break;
 				case LOGIN:
-					String[] split = builder.toString().split("\\" + (char) ((byte) 0));
+					String[] split = builder.toString().split("\\" + (char) ((byte) DELIMITER));
 					try {
 						usermanager.userLogin(split[0], split[1], socket);
 					} catch (UserException e) {
 						try {
 							usermanager.userRegistration(split[0], split[1]);
 						} catch (UserAlreadyExistsException e1) {
-							// TODO sende passwort falsch
-
 							new MessageSender(socket).sendMessage("Das eingegebene Passwort ist falsch!".getBytes(),
 									MessageListener.ERROR_MESSAGE);
-
+						}
+						try {
+							usermanager.userLogin(split[0], split[1], socket);
+						} catch (UserException e1) {
+							new MessageSender(socket).sendMessage("Fehler!".getBytes(), MessageListener.ERROR_MESSAGE);
 						}
 					}
 					break;
--- src/server/Test.java
+++ src/server/Test.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 public class Test {
 
 	public static void main(String[] args) {
+		System.out.println("Server");
 		Server s = new Server(12345, "users.csv");
 		try {
 			s.start();
