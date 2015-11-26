98cfcf62214a306eae313dd66b9ffb36c9a06038
jens7841 <jens7841@web.de>
2015-11-26 14:11:27 +0100

    .

---
+++ src/server/MessageHandler.java
@@ -0,0 +1,50 @@
+package server;
+
+import chatshared.Messages;
+import usermanager.UserAlreadyExistsException;
+import usermanager.UserAlreadyLoggedInException;
+import usermanager.UserException;
+
+public class MessageHandler {
+
+	public MessageHandler() {
+
+	}
+
+	public void login(String message) {
+		String[] split = builder.toString().split("\\" + (char) ((byte) Messages.DELIMITER));
+		try {
+			if (split.length < 2) {
+				new MessageSender(socket).sendMessage("LoginMessageFehler", Messages.LOGIN_ERROR_MESSAGE);
+			} else {
+				user = userManager.loginUser(split[0], split[1], socket);
+				new MessageSender(socket).sendMessage("Login Erfolgreich", Messages.LOGIN_SUCCESS_MESSAGE);
+
+			}
+
+		} catch (UserAlreadyLoggedInException e1) {
+			new MessageSender(socket).sendMessage("User ist bereits eingeloggt!", Messages.LOGIN_ERROR_MESSAGE);
+
+		} catch (UserException e) {
+			try {
+				userManager.registerUser(split[0], split[1]);
+				new MessageSender(socket).sendMessage("Registrierung erfolgreich!", Messages.SUCCESS_MESSAGE);
+
+				try {
+					user = userManager.loginUser(split[0], split[1], socket);
+					new MessageSender(socket).sendMessage("Login Erfolgreich", Messages.LOGIN_SUCCESS_MESSAGE);
+				} catch (UserException e1) {
+					new MessageSender(socket).sendMessage("Fehler beim einloggen nach dem Registrieren!",
+							Messages.ERROR_MESSAGE);
+				}
+
+			} catch (UserAlreadyExistsException e1) {
+				new MessageSender(socket).sendMessage("Das eingegebene Passwort ist falsch!",
+						Messages.LOGIN_ERROR_MESSAGE);
+			} catch (UserException e2) {
+				new MessageSender(socket).sendMessage(e2.getMessage(), Messages.LOGIN_ERROR_MESSAGE);
+			}
+		}
+	}
+
+}
