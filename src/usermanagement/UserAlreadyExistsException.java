52195b2727e06c8bd08986b79604f98c0e08d683
clemens961 <clemens961@gmail.com>
2015-12-03 15:48:18 +0100

    UserAlreadyExistsException

---
+++ src/usermanagement/UserAlreadyExistsException.java
@@ -0,0 +1,14 @@
+package usermanagement;
+
+public class UserAlreadyExistsException extends UserException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4355323869177447601L;
+
+	public UserAlreadyExistsException(String message) {
+		super(message);
+	}
+
+}
