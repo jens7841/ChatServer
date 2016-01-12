5fe19282092b33b9019c8fcd33dc383d1638baa2
clemens961 <clemens961@gmail.com>
2016-01-12 16:13:08 +0100

    usernamen anzeigen (jetzt wörklich)

--- src/messagehandling/messagehandler/ChatMessageHandler.java
+++ src/messagehandling/messagehandler/ChatMessageHandler.java
@@ -4,14 +4,16 @@ import java.io.UnsupportedEncodingException;
 
 import messagehandling.Message;
 import server.ConnectionHandler;
+import usermanagement.User;
 
 public class ChatMessageHandler implements MessageHandler {
 
 	@Override
 	public void handleMessage(Message message, ConnectionHandler connectionHandler) {
 		try {
-			if (connectionHandler.getUser() != null) {
-				System.out.println(new String(message.getBytes(), "UTF-8"));
+			User user = connectionHandler.getUser();
+			if (user != null) {
+				System.out.println("> " + user.getName() + ": " + new String(message.getBytes(), "UTF-8"));
 			}
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
