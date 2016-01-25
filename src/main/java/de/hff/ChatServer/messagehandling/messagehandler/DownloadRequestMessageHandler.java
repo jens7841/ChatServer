51227418326563f80c596232709d29856d73b470
clemens961 <clemens961@gmail.com>
2016-01-25 16:09:07 +0100

    DownloadRequestMessageHandler erstellt

---
+++ src/main/java/de/hff/ChatServer/messagehandling/messagehandler/DownloadRequestMessageHandler.java
@@ -0,0 +1,13 @@
+package de.hff.ChatServer.messagehandling.messagehandler;
+
+import de.hff.ChatServer.usermanagement.UserHandler;
+import de.hff.ChatShared.messagehandling.Message;
+
+public class DownloadRequestMessageHandler implements MessageHandler {
+
+	@Override
+	public void handleMessage(Message message, UserHandler userHandler) {
+		// TODO Auto-generated method stub
+	}
+
+}
