144a991c490b4c79a9ba80f9b6906b24bbc7fab1
clemens961 <clemens961@gmail.com>
2016-01-28 14:01:55 +0100

    disconnect mh

--- src/main/java/de/hff/ChatServer/messagehandling/messagehandler/DisconnectMessageHandler.java
+++ src/main/java/de/hff/ChatServer/messagehandling/messagehandler/DisconnectMessageHandler.java
@@ -5,17 +5,27 @@ import de.hff.ChatServer.usermanagement.UserHandler;
 import de.hff.ChatServer.usermanagement.UserManager;
 import de.hff.ChatShared.messagehandling.Message;
 import de.hff.ChatShared.messagehandling.MessageHandler;
+import de.hff.ChatShared.messagehandling.MessageType;
 
 public class DisconnectMessageHandler implements MessageHandler {
 
 	private UserManager userManager;
+	private UserHandler userHandler;
 
-	public DisconnectMessageHandler(UserManager userManager) {
+	public DisconnectMessageHandler(UserManager userManager, UserHandler userHandler) {
 		this.userManager = userManager;
+		this.userHandler = userHandler;
 	}
 
 	@Override
-	public void handleMessage(Message message, UserHandler userHandler) {
+	public void receiveMessage(Message message) {
+		if (message.getType() == MessageType.DISCONNECT) {
+			handleMessage(message);
+		}
+	}
+
+	@Override
+	public void handleMessage(Message message) {
 		User user = userHandler.getUser();
 		if (user != null) {
 			userManager.logout(user);
